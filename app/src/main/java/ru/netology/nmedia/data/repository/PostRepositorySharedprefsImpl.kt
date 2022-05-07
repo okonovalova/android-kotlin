21dd99edf2e5802891f7a060a20773c38bd49307
okonovalova <zaynullina.olya@mail.ru>
2022-05-07 19:34:43 +0500

    Домашнее задание к занятию «3.3 Хранение данных»

---
+++ app/src/main/java/ru/netology/nmedia/data/repository/PostRepositorySharedprefsImpl.kt
@@ -0,0 +1,41 @@
+package ru.netology.nmedia.data.repository
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import ru.netology.nmedia.data.model.PostInfo
+
+class PostRepositorySharedPrefsImpl(context: Context) : PostRepository {
+    private val gson = Gson()
+    private val prefs = context.getSharedPreferences("repo", Context.MODE_PRIVATE)
+    private val type = TypeToken.getParameterized(List::class.java, PostInfo::class.java).type
+    private val key = "posts"
+    private var posts = emptyList<PostInfo>()
+    private val data = MutableLiveData(posts)
+
+    init {
+        prefs.getString(key,null)?.let {
+            posts = gson.fromJson(it,type)
+            data.value = posts
+        }
+    }
+
+    private fun sync() {
+        with(prefs.edit()) {
+            putString(key, gson.toJson(data.value))
+            apply()
+        }
+    }
+
+    override fun getPostsData(): LiveData<List<PostInfo>> {
+        return data
+    }
+
+    override fun updatePostsData(postsInfo: List<PostInfo>) {
+        data.value = postsInfo
+        sync()
+    }
+
+}
